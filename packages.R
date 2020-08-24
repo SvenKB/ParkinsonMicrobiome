69d070ef93071797bd660f987882fe9588fbfe80
Sven Kleine Bardenhorst <s.bardenhorst@me.com>
2020-08-24 08:54:48 +0200

    code update

--- packages.R
+++ packages.R
@@ -2,11 +2,11 @@
 packages <- c("ggplot2","tidyverse","drake",'rjson','knitr','kableExtra',
               'readr','hablar','foreach','phyloseq','vegan','lme4','microbiome',
               'merTools',"sjPlot",'lmerTest','reshape2','sjmisc','sjmisc',
-              'texreg','ape','microbiome')
+              'texreg','ape','microbiome','rmdformats','seqinr','hillR','metafor')
 
 #### Install packages ####
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
-  install.packages(setdiff(packages, rownames(installed.packages())))  
+  install.packages(setdiff(packages, rownames(installed.packages())),repos = "http://cran.us.r-project.org")  
 }
 
 #### Load packages ####
